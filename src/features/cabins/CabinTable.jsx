1a095bfbf52bed1616fd5e8cc837ef614f90055c
ShomyMagi <milos.simic002@gmail.com>
2025-06-04 09:58:40 +0200

    Adds filtering, sorting and pagination features for the booking data

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -2,6 +2,7 @@ import CabinRow from "./CabinRow";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import Empty from "../../ui/Empty";
 import { useCabins } from "./useCabins";
 import { useSearchParams } from "react-router-dom";
 
@@ -10,6 +11,8 @@ function CabinTable() {
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  if (!cabins.length) return <Empty resourceName="cabins" />;
 
   // 1. Filter
   const filterValue = searchParams.get("discount") || "all";
